1901e35a1e23e3c8c275500180b4594da1d8576a
Tito Luyo Murata <tluyo@hotmail.com>
2016-01-31 02:12:27 -0500

    commit inicial

---
+++ heatmap.js
@@ -0,0 +1,39 @@
+﻿var map;
+function initialize() {
+
+    var sanFrancisco = new google.maps.LatLng(37.7835, -122.441);
+
+    var lima = new google.maps.LatLng(-12.100757399999999, -77.0275498);
+
+    var canvas = document.getElementById('map-canvas');
+    var mapOptions = {
+        zoom: 16,
+        center : sanFrancisco
+    };
+    map = new google.maps.Map(canvas,mapOptions);
+
+    // Heatmap layer
+    $.getJSON('heatmapdata.json', function (data) {
+
+        var dataLength = data.length;
+        var heatMapData = [];
+        for (var i = 0; i < dataLength; i++) {
+            // Este el formato de datos con "peso"
+            // revisar https://developers.google.com/maps/documentation/javascript/reference#WeightedLocation
+            heatMapData.push({
+                location: new google.maps.LatLng(data[i].pos),
+                weight: data[i].weight
+            });
+        }
+
+        var heatmap = new google.maps.visualization.HeatmapLayer({
+            data: heatMapData
+            , radius: 30
+            //, opacity: 0.5
+            //, gradient: gradient
+        });
+        heatmap.setMap(map);
+    });
+    
+}
+google.maps.event.addDomListener(window, 'load', initialize);
